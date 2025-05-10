979cd7f13fa95410963720d9010e9dca5231b9c5
hishmatrai <hishmatrai.2018@gmail.com>
2025-05-10 18:46:45 +0500

    update

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,11 +1,13 @@
 const firebaseConfig = {
   apiKey: "AIzaSyD04xNun9Z6ybSJJpjSWt2QYfunqTROQUQ",
   authDomain: "testing-cf5e2.firebaseapp.com",
+  databaseURL: "https://testing-cf5e2-default-rtdb.firebaseio.com",
   projectId: "testing-cf5e2",
   storageBucket: "testing-cf5e2.firebasestorage.app",
   messagingSenderId: "401500897709",
   appId: "1:401500897709:web:5317f82914075e47ffe124",
   measurementId: "G-JNZQD7KSWY",
 };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
--- js/home.js
+++ js/home.js
@@ -9,7 +9,6 @@ firebase.auth().onAuthStateChanged((user) => {
     window.location.assign("login.html");
   }
 });
-
 const LogOut = () => {
   //
   firebase
@@ -19,3 +18,67 @@ const LogOut = () => {
       window.location.assign("login.html");
     });
 };
+
+// add data
+const input = document.getElementById("input");
+function uuidv4() {
+  return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, (c) =>
+    (
+      +c ^
+      (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (+c / 4)))
+    ).toString(16)
+  );
+}
+
+const addHandler = () => {
+  // Ex : 1
+  // firebase
+  //   .database()
+  //   .ref("todos/" + "todo1")
+  //   .set({
+  //     value: input.value,
+  //   })
+  //   .then(() => {
+  //     input.value = "";
+  //   });
+
+  // Ex : 2
+  // firebase
+  //   .database()
+  //   .ref("todos/" + uuidv4())
+  //   .set({
+  //     value: input.value,
+  //   })
+  //   .then(() => {
+  //     input.value = "";
+  //   });
+
+  // Ex: 3
+  firebase
+    .database()
+    .ref("todos/")
+    .push({
+      value: input.value,
+    })
+    .then(() => {
+      input.value = "";
+    });
+  // add update
+};
+
+// get data
+// firebase
+//   .database()
+//   .ref("todos/" + "-OPuOSIlmtwFr9wfezKb")
+//   .on("value", (res) => {
+//     console.log("res", res.val());
+//   });
+
+firebase
+  .database()
+  .ref("users/")
+  .on("value", (res) => {
+    res.forEach((data) => {
+      console.log("data", data.val());
+    });
+  });
